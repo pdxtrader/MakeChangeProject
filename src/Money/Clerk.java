55900b59049dce567b32facc2b855689f8e42fe5
pdxtrader <benjaminblodgett@gmail.com>
2017-04-08 17:50:13 -0600

    Added recursive loop

--- src/Money/Clerk.java
+++ src/Money/Clerk.java
@@ -1,96 +1,132 @@
 package Money;
+
 import java.util.Scanner;
 
 public class Clerk {
 	public static void main(String[] args) {
-		
-		float itemPrice = 0.00F;
-		int amountTendered = 0;
-		
-		
-		
-		
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter the price of the product to be purchased: " );
-		    itemPrice = kb.nextFloat();
-		    
-		System.out.println("Enter the amount of cash the customer tendered: " );
-		    amountTendered = kb.nextInt();
-		
-		    
-		    public class For {
-		     public static void main(String[] args){
-		              for(int i=2; i<=1024; i *= 2){
-		               System.out.println("Count is: " + i);
-		          }
-		     }
 
+		float itemPrice = 0.00F;
+		float amountTendered = 0.00F;
+		float[] giveMe = new float[2];
 
+		giveMe = queryClerk(kb);
+		itemPrice = giveMe[0];
+		amountTendered = giveMe[1];
+		System.out.println(giveMe);
 
+		// public class DoWhile {
+		// int i = 1;
+		// if (i < 1024) {
+		// do { i*=2;
+		// System.out.println("Count is: " + i);
+		// } while (i < 1024);
+	}// end main
 
-		    public class While {
-		        public static void main(String[] args){
-		            int i = 1;
-		            while (i < 1024) {
-		                i *= 2;
-		                System.out.println("Count is: " + i);
-		          }
-		     }
+	public static float[] queryClerk(Scanner s) {
+		System.out.println("Enter the price of the product to be purchased: ");
+		float itemPrice = s.nextFloat();
 
+		System.out.println("Enter the amount of cash the customer tendered: ");
+		float amountTendered = s.nextFloat();
 
-		    public class DoWhile {
-		         public static void main(String[] args){
-		            int i = 1;
-		            if (i < 1024) {
-		                do { i*=2;
-		                    System.out.println("Count is: " + i);
-		                } while (i < 1024);
-		            }
-		         }
-		
-		    void loop(int i, int j, int k) {        
-		        list.add(array[i] + array[j] + array[k]);
+		float[] arrayQuery = new float[2];
+		arrayQuery[0] = itemPrice;
+		arrayQuery[1] = amountTendered;
 
-		        // advance states
-		        k++;
-		        if (k == x) { k = 0; j++; }
-		        if (j == x) { j = 0; i++; }
-		        if (i == x) { i = 0; }
-
-		        // terminate on all wrap-around
-		        if (i == 0 && j == 0 && k == 0) { return; }
-
-		        // recurse
-		        loop(i, j, k);
-		    }
-		    
-		    
-
-		System.out.println(itemPrice);
-		System.out.println(amountTendered);
-
-	}
+		if (itemPrice > amountTendered) {
+			System.out.println();
+			System.out.println("*****ERROR*****ERROR*****ERROR*****ERROR*****");
+			System.out.printf("The purchase price of $%.2f is greater than ", itemPrice);
+			System.out.println();
+			System.out.printf("the amount tendered of $%.2f, please correct\n", amountTendered);
+			System.out.println("*****ERROR*****ERROR*****ERROR*****ERROR*****");
+			System.out.println();
+			arrayQuery = queryClerk(s);
+			return arrayQuery;
+		} else {
+			return arrayQuery;
+		}
+	}// end clerk query method
 }
 
-//The user is then prompted asking how much money was tendered by the customer.
+// User Story #3
 //
-//User Story #3
+// Display an appropriate message if the customer provided too little money or
+// the exact amount.
 //
-//Display an appropriate message if the customer provided too little money or the exact amount.
+// User Story #4
 //
-//User Story #4
+// If the amount tendered is more than the cost of the item, display the number
+// of bills and coins that should be given to the customer.
+
+// public class For {
+// public static void main(String[] args){
+// for(int i=2; i<=1024; i *= 2){
+// System.out.println("Count is: " + i);
+// }
+// }
 //
-//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
+//
+//
+//
+// public class While {
+// public static void main(String[] args){
+// int i = 1;
+// while (i < 1024) {
+// i *= 2;
+// System.out.println("Count is: " + i);
+// }
+// }
+//
+//
+// public class DoWhile {
+// public static void main(String[] args){
+// int i = 1;
+// if (i < 1024) {
+// do { i*=2;
+// System.out.println("Count is: " + i);
+// } while (i < 1024);
+// }
+// }
+//
+// void loop(int i, int j, int k) {
+// list.add(array[i] + array[j] + array[k]);
+//
+// // advance states
+// k++;
+// if (k == x) { k = 0; j++; }
+// if (j == x) { j = 0; i++; }
+// if (i == x) { i = 0; }
+//
+// // terminate on all wrap-around
+// if (i == 0 && j == 0 && k == 0) { return; }
+//
+// // recurse
+// loop(i, j, k);
+// }
 
 //
-//[2:27]  
-//System.out.printf("%.2f", change);
+// System.out.println(itemPrice);
+// System.out.println(amountTendered);
 //
-//[2:28]  
-//It prints out the double to two sig figs
+// }
+//
+// private static void queryClerk(float itemPrice, int amountTendered) {
+// // TODO Auto-generated method stub
 
-//char first = '\u0042';
-//char middle = '\u0053';
-//char last = '\u0042';
+// }
+// }
+
 //
-//System.out.println("Intials: " + first + middle + last);
+// [2:27]
+// System.out.printf("%.2f", change);
+//
+// [2:28]
+// It prints out the double to two sig figs
+
+// char first = '\u0042';
+// char middle = '\u0053';
+// char last = '\u0042';
+//
+// System.out.println("Intials: " + first + middle + last);
