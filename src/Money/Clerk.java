15941426a12cefd42ab6b06354da1b7b3f33bd4f
pdxtrader <benjaminblodgett@gmail.com>
2017-04-09 19:02:37 -0600

    Final code comments and formatting

--- src/Money/Clerk.java
+++ src/Money/Clerk.java
@@ -10,18 +10,23 @@ public class Clerk {
 		float amountTendered = 0.00F;
 		float[] giveMe = new float[2];
 
-		giveMe = queryClerk(kb);
+		//defines which Scanner to send to our queryClerk method
+		giveMe = queryClerk(kb);  //saves input from our query & error method
 		itemPrice = giveMe[0];
 		amountTendered = giveMe[1];
 		System.out.printf("The change due is $%.2f\n", (amountTendered - itemPrice));
 
+		//casts the floats into ints post calculation and times by 100 to match the
+		//cents nomenclature used by denomBreakdown
 		int changeDue = (int) ((amountTendered - itemPrice) * 100);
 		System.out.println();
 
+		//utilizes two String arrays for singular and plural denominations
 		String[] bill = { "Hundred", "Fifty", "Twenty", "Ten", "Five", "One", "Quarter", "Dime", "Nickel", "Penny" };
 		String[] bills = { "Hundreds", "Fifties", "Twenties", "Tens", "Fives", "Ones", "Quarters", "Dimes", "Nickels",
 				"Pennies" };
 
+		//denominates in cents as to avoid rounding errors
 		int[] denomBreakdown = new int[10];
 		denomBreakdown[0] = 10000; 	// this is for 100 dollars
 		denomBreakdown[1] = 5000;
@@ -34,6 +39,8 @@ public class Clerk {
 		denomBreakdown[8] = 5;
 		denomBreakdown[9] = 1;		//this is for a penny
 
+		//for else loop that that calculates the quantity of each denomination required
+		//used dynamic conditional fields so if array changed length it wouldn't break
 		for (int i = 0; i < denomBreakdown.length; i++) { // if plural
 			if ((changeDue / denomBreakdown[i]) > 1) {
 				System.out.println("The customer is owed: " + changeDue / denomBreakdown[i] + " " + bills[i]);
@@ -47,7 +54,9 @@ public class Clerk {
 		}
 
 	}
-
+	
+	//recursive query that reads in a Scanner, calls its own method, 
+	//controls for data errors, and returns the change due
 	public static float[] queryClerk(Scanner s) {
 		System.out.println("Enter the price of the product to be purchased: ");
 		float itemPrice = s.nextFloat();
@@ -70,9 +79,9 @@ public class Clerk {
 			System.out.println("*****ERROR*****ERROR*****ERROR*****ERROR*****");
 			System.out.println();
 			arrayQuery = queryClerk(s);
-			return arrayQuery;
+			return arrayQuery;  //this would never be used but needs to be there for the method to work
 		} else {
 			return arrayQuery;
 		}
 	}// end clerk query method
-}
+}// end main method
